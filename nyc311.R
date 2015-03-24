d76475d9c2b2ca5e5ada8f05820658389dc93329
jcp1016 <jcp1016@mac.com>
2015-03-24 12:03:29 -0400

    added heatmaps

--- nyc311.R
+++ nyc311.R
@@ -1,41 +1,115 @@
 require("qdapTools")
+require("reshape")
+require("gplots")
+require("RColorBrewer")
+require("dplyr")
 
 ## Add categories for poverty data
 poverty_data$FamPvCategory  <- cut(poverty_data$FamBwPvP,   breaks=c(0,10,20,30,40,50,60), labels=c( "(0-10%]", "(10-20%]","(20-30%]", "(30-40%]", "(40-50%]", "(50-60%]" ), include.lowest=TRUE)
 poverty_data$ChldPvCategory <- cut(poverty_data$FCU18BwPvP, breaks=c(0,10,20,30,40,50,60), labels=c( "(0-10%]", "(10-20%]","(20-30%]", "(30-40%]", "(40-50%]", "(50-60%]" ), include_lowest=TRUE)
 poverty_data$EldPvCategory  <- cut(poverty_data$P65plBwPvP, breaks=c(0,10,20,30,40,50,60), labels=c( "(0-10%]", "(10-20%]","(20-30%]", "(30-40%]", "(40-50%]", "(50-60%]" ), include_lowest=TRUE)
+poverty_all_df  <- poverty_data[,c(2,4,43)]
+poverty_chld_df <- poverty_data[,c(2,4,44)]
+poverty_eld_df  <- poverty_data[,c(2,4,45)]
 setwd("~/Columbia/nyc-311")
 
 ## Attach PUMA IDs to 311 data based on a lookup
-setwd("~/Columbia/nyc-311/DATA")
 zipcode_to_puma <- read.csv("nyc_zcta10_to_puma10.csv", stringsAsFactors=FALSE)
 zipcode_to_puma$zcta10 <- as.character(zipcode_to_puma$zcta10)
 nyc311_df$PUMA_ID <- lookup(nyc311_df$Zipcode, zipcode_to_puma[,c(1,4)])
 nyc311_df$PUMA_ID <- as.factor(nyc311_df$PUMA_ID)
+names(nyc311_df)[6] <- "ComplaintType"
 
-## Aggregate complaints by poverty category
-#by_puma_cat <- group_by( nyc311_df, PUMA_ID, `Complaint.Type`, Descriptor )
-#nyc311_aggr <- summarize( by_puma_cat, PUMA_ID, n=n() )
-#data_by_day   <- na.omit(d2)
+## Clean up complaint type
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "APPLIANCE"] <- "Appliance"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "GENERAL CONSTRUCTION"] <- "Construction"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "CONSTRUCTION"] <- "Construction"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "Derelict Vehicles"] <- "Derelict Vehicle"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "ELECTRIC"] <- "Electric"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "GENERAL"] <- "General"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "HEATING"] <- "Heating"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "NONCONST"] <- "Non construction"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "PAINT - PLASTER"] <- "Paint/plaster"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "PAINT/PLASTER"] <- "Paint/plaster"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "PLUMBING"] <- "Plumbing"
+nyc311_df$ComplaintType[nyc311_df$ComplaintType == "STRUCTURAL"] <- "Structural"
+nyc311_df$ComplaintType[substr(nyc311_df$ComplaintType,1,5)  == "Noise"] <- "Noise"
+nyc311_df$ComplaintType[substr(nyc311_df$ComplaintType,1,11) == "Street Sign"] <- "Street Sign Damaged/Missing"
+nyc311_df$ComplaintType[substr(nyc311_df$ComplaintType,1,10) == "Fire Alarm"] <- "Fire Alarm Addn/Modif/Insp"
+nyc311_df$ComplaintType[substr(nyc311_df$ComplaintType,1,12) == "Highway Sign"] <- "Highway Sign Damaged/Missing"
 
-by_cat <- group_by( nyc311_aggr, PUMA_ID, `Complaint.Type` )
-nyc311_cat <- summarize( by_cat, n=sum(n) )
+## Aggregate by complaint type
+by_cat <- group_by( nyc311_df, PUMA_ID, ComplaintType )
+nyc311_cat <- summarize( by_cat, n=n() )
 
-#by_cat <- group_by( nyc311_cat, `Complaint.Type` )
-#nyc311_rank <- summarize( by_cat, n=sum(n) )
+## Merge aggregated complaints with poverty stats by PUMA ID
+data <- merge(nyc311_cat, poverty_all_df, by="PUMA_ID")
+names(data)[5] <- "PvCategory"
 
-#s1 <- nyc311_aggr %>%
-#        select(PUMA_ID, n, `Complaint.Type`, Descriptor) %>%
-#        arrange(PUMA_ID, desc(n))
+## Aggregate by poverty category and complaint type
+by_pvcat <- group_by( data, PvCategory, ComplaintType )
+s1 <- summarize( by_pvcat, n=sum(n) )
+t1 <- melt(s1)
+t2 <- cast(t1, ComplaintType ~ PvCategory, sum, margins=TRUE)
+names(t2)[ncol(t2)] <- "Total"
+t3 <- filter(t2, Total > 5000)
+t4 <- t3[order(desc(t3$Total)),]
+t4 <- t4[-1,]
+row.names(t4) <- t4$ComplaintType
+t4 <- t4[,-1]
+t4 <- t4[,-ncol(t4)]  
+data_matrix <- data.matrix(scale(t4))
+pdf("heatmap_lvl.pdf", width=10, height=12)
+heatmap.2(data_matrix,  
+          Rowv=NA, Colv=NA, 
+          dendrogram='none',
+          scale='none', 
+          col=brewer.pal(9,"Blues"), 
+          main="What Calls to 311 \nReveal About New York",
+          key=FALSE, 
+          keysize=1, 
+          srtCol=0, 
+          trace='none', 
+          offsetRow = 0.5,
+          offsetCol = 0.5,
+          margins=c(5,12),
+          xlab="\nPoverty level based on community district of caller",
+          ylab=NULL,
+          colsep,
+          rowsep,
+          sepcolor='white',
+          sepwidth=c(0.1,0.1))
 
-s2 <- nyc311_cat %>%
-        select( PUMA_ID, n, `Complaint.Type` ) %>%
-        arrange( PUMA_ID, desc(n) )
+dev.off()
 
-s3 <- nyc311_rank %>%
-        arrange( desc(n) )
-
-setwd("~/Columbia/nyc-311")
-
-
-## 
+## Aggregate by community district and complaint type
+by_dc <- group_by( data, CD_Name, ComplaintType )
+s1 <- summarize( by_dc, n=sum(n) )
+t1 <- melt(s1)
+t2 <- cast(t1, ComplaintType ~ CD_Name, sum, margins=TRUE)
+names(t2)[ncol(t2)] <- "Total"
+t3 <- filter(t2, Total > 5000)
+t4 <- t3[order(desc(t3$Total)),]
+t4 <- t4[-1,]
+row.names(t4) <- t4$ComplaintType
+t4 <- t4[,-1]
+t4 <- t4[,-ncol(t4)]  
+data_matrix <- data.matrix(scale(t4))
+pdf("heatmap_cd.pdf", width=10, height=14)
+heatmap.2(data_matrix, 
+          Rowv=NA, Colv=NA, 
+          dendrogram='none',
+          scale='none', 
+          col=brewer.pal(9,"Blues"), 
+          #main="311 Calls by Community",
+          key=FALSE, 
+          keysize=1, 
+          trace='none', 
+          offsetRow = 0.5,
+          offsetCol = 0.5,
+          margins=c(16,12),
+          #margins=c(12,5),
+          xlab=NULL,
+          ylab=NULL)
+dev.off()
+                 
