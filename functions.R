362687e4706c3fb2edc115a6e03cfada742ac0c6
Janet Prumachuk <jcprumachuk@mac.com>
2015-03-30 23:11:12 -0400

    cleanup

--- functions.R
+++
@@ -1,5 +0,0 @@
-tractLookup <- function(x, y, state) {
-        pt <- SpatialPoints(data.frame(x = x, y = y))
-        overlay.pt <- overlay(pt, state) # what index number does pt fall inside?
-        return(census$TRACT[overlay.pt]) # give the Tract number from the census layer
-}
